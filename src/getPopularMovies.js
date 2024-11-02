ad3213a6cab65a353444ae04a49db3f5ed00e83d
AbedinTelaku <abedintelaku1@gmail.com>
2024-11-03 00:29:42 +0100

    Organization code in index.js

--- src/getPopularMovies.js
+++ src/getPopularMovies.js
@@ -1,27 +1,19 @@
-// require('dotenv').config();
-// console.log('API Key:', process.env.API_KEY);
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
 
-// const express = require('express');
-// const cors = require('cors');
-// const fetch = require('node-fetch');
-// const app = express();
-// const PORT = process.env.PORT || 5000;
+dotenv.config();
 
-// app.use(cors({ origin: 'http://localhost:3000' }));
+const getPopularMovies = async (req, res) => {
+  try {
+    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
+    if (!response.ok) {
+      throw new Error('Failed to fetch films');
+    }
+    const data = await response.json();
+    res.json(data.results);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
 
-// app.get('/api/popular-movies', async (req, res) => {
-//   try {
-//     const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch films');
-//     }
-//     const data = await response.json();
-//     res.json(data.results);
-//   } catch (error) {
-//     res.status(500).json({ message: error.message });
-//   }
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
+export default getPopularMovies;
--- src/index.js
+++ src/index.js
@@ -1,28 +1,16 @@
 import express from 'express';
 import cors from 'cors';
-import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import getPopularMovies from './getPopularMovies.js';
 
 dotenv.config();
-console.log('API Key:', process.env.API_KEY);
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({ origin: 'http://localhost:3000' }));
 
-app.get('/api/popular-movies', async (req, res) => {
-  try {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`);
-    if (!response.ok) {
-      throw new Error('Failed to fetch films');
-    }
-    const data = await response.json();
-    res.json(data.results);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+app.get('/api/popular-movies', getPopularMovies);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
